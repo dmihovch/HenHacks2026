17e948d84941f84b0919f49ed9939d8cfa980797
Dan Mihovch <dmihovch@gmail.com>
2026-02-28 16:26:01 -0500

    in a good spot

--- include/quickdraw.h
+++ include/quickdraw.h
@@ -1,4 +1,33 @@
 #include <raylib.h>
 #include "constants.h"
 
+typedef enum 
+{
+	NO_SHOT,
+	MISS,
+	HIT,
+} LastShot;
+typedef struct {
+    Vector2 pos;
+    Vector2 vel;
+    float length;
+    float thickness;
+    Color col;
+    int key_up;
+    int key_down;
+    int key_left;
+    int key_right;
+    int key_shoot;
+	LastShot shot;
+	bool locked;
+} Crosshair;
+
+typedef struct {
+    Vector2 pos;
+    float radius;
+    Color col;
+} Target;
+
+
 void enterQuickdraw();
+
