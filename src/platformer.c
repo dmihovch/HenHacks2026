34bea8d2a584042ce8232320de2a4a5b03ffb0fc
JACK MALLETT <JACKMUD@UDEL.EDU>
2026-02-28 16:26:04 -0500

    added shooter functionality

--- src/platformer.c
+++ src/platformer.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 960
@@ -12,35 +13,43 @@
 #define GRAVITY 0.8f
 
 #define PLATFORM_COUNT 30
-
 #define PLATFORM_WIDTH 500
 #define PLATFORM_HEIGHT 20
-
 #define VERTICAL_SPACING 140
 
-// #define BASE_MAX_HORIZONTAL_SHIFT 550
-// #define MAX_ALLOWED_SHIFT 1000
-// #define MIN_HORIZONTAL_SHIFT 500
+#define MAX_BULLETS 250
+#define BULLET_SPEED 8
+#define SHOOT_INTERVAL 120
 
 typedef struct {
     Rectangle rect;
     Rectangle dropZone;
 } Platform;
 
+typedef struct {
+    Rectangle rect;
+    int direction;
+    int shootTimer;
+    bool active;
+} Shooter;
+
+typedef struct {
+    Vector2 pos;
+    int direction;
+    bool active;
+} Bullet;
+
 void GeneratePlatforms(Platform platforms[])
 {
     float baseX = 400;
     float baseY = 850;
 
-    // Bottom platform (fixed)
     platforms[0].rect = (Rectangle){
-        baseX,
-        baseY,
-        PLATFORM_WIDTH,
-        PLATFORM_HEIGHT
+        baseX, baseY,
+        PLATFORM_WIDTH, PLATFORM_HEIGHT
     };
 
-    platforms[0].dropZone = (Rectangle){ 0, 0, 0, 0 };
+    platforms[0].dropZone = (Rectangle){0,0,0,0};
 
     float prevX = baseX;
     float prevY = baseY;
@@ -49,50 +58,33 @@ void GeneratePlatforms(Platform platforms[])
     for (int i = 1; i < PLATFORM_COUNT; i++)
     {
         float newWidth = GetRandomValue(260, 500);
-
-        // Controls how much the platforms overlap horizontally
-        // Lower overlap = harder jump
         float overlap = GetRandomValue(80, 160);
-
-        // Enforce strong horizontal displacement
         float minSeparation = 450;
 
-        int direction = GetRandomValue(0, 1) == 0 ? -1 : 1;
-
+        int direction = GetRandomValue(0,1) == 0 ? -1 : 1;
         float newX;
 
         if (direction == 1)
         {
-            // Spawn to the right
             newX = prevX + prevWidth - overlap;
-
-            // Ensure not stacking too close
             if (fabs(newX - prevX) < minSeparation)
                 newX = prevX + minSeparation;
         }
         else
         {
-            // Spawn to the left
             newX = prevX - newWidth + overlap;
-
             if (fabs(newX - prevX) < minSeparation)
                 newX = prevX - minSeparation;
         }
 
-        // Clamp inside bounds
-        if (newX < 100)
-            newX = 100;
-
-        if (newX + newWidth > 1180)
-            newX = 1180 - newWidth;
+        if (newX < 100) newX = 100;
+        if (newX + newWidth > 1180) newX = 1180 - newWidth;
 
         float newY = prevY - VERTICAL_SPACING;
 
         platforms[i].rect = (Rectangle){
-            newX,
-            newY,
-            newWidth,
-            PLATFORM_HEIGHT
+            newX, newY,
+            newWidth, PLATFORM_HEIGHT
         };
 
         platforms[i].dropZone = (Rectangle){
@@ -108,24 +100,72 @@ void GeneratePlatforms(Platform platforms[])
     }
 }
 
+void RegenerateShooters(Platform platforms[], Shooter shooters[])
+{
+    for (int i = 0; i < PLATFORM_COUNT; i++)
+    {
+        shooters[i].active = false;
+        shooters[i].shootTimer = 0;
+
+        if (i == 0) continue;
+
+        float spawnChance = (float)i / PLATFORM_COUNT;
+
+        if (GetRandomValue(0,100) < spawnChance * 100)
+        {
+            shooters[i].active = true;
+
+            Rectangle p = platforms[i].rect;
+            bool spawnLeft = GetRandomValue(0,1) == 0;
+
+            if (spawnLeft)
+            {
+                shooters[i].rect = (Rectangle){
+                    p.x - 20,
+                    p.y - 40,
+                    20,
+                    40
+                };
+                shooters[i].direction = 1;
+            }
+            else
+            {
+                shooters[i].rect = (Rectangle){
+                    p.x + p.width,
+                    p.y - 40,
+                    20,
+                    40
+                };
+                shooters[i].direction = -1;
+            }
+        }
+    }
+}
+
 void enterPlatformer(void)
 {
     srand(time(NULL));
 
     Platform platforms[PLATFORM_COUNT];
+    Shooter shooters[PLATFORM_COUNT];
+    Bullet bullets[MAX_BULLETS];
+
     GeneratePlatforms(platforms);
+    RegenerateShooters(platforms, shooters);
+
+    for (int i = 0; i < MAX_BULLETS; i++)
+        bullets[i].active = false;
 
     Vector2 playerPos = {
-        platforms[0].rect.x + PLATFORM_WIDTH / 2 - PLAYER_WIDTH / 2,
+        platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2,
         platforms[0].rect.y - PLAYER_HEIGHT
     };
 
-    Vector2 velocity = { 0, 0 };
+    Vector2 velocity = {0,0};
     bool onGround = true;
 
-    Camera2D camera = { 0 };
-    camera.offset = (Vector2){ SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f };
-    camera.rotation = 0.0f;
+    Camera2D camera = {0};
+    camera.offset = (Vector2){SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
     camera.zoom = 1.0f;
 
     SetTargetFPS(60);
@@ -138,11 +178,14 @@ void enterPlatformer(void)
         if (IsKeyPressed(KEY_R))
         {
             GeneratePlatforms(platforms);
+            RegenerateShooters(platforms, shooters);
 
-            playerPos.x = platforms[0].rect.x + PLATFORM_WIDTH / 2 - PLAYER_WIDTH / 2;
+            for (int i = 0; i < MAX_BULLETS; i++)
+                bullets[i].active = false;
+
+            playerPos.x = platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2;
             playerPos.y = platforms[0].rect.y - PLAYER_HEIGHT;
-
-            velocity = (Vector2){ 0, 0 };
+            velocity = (Vector2){0,0};
             onGround = true;
         }
 
@@ -159,7 +202,6 @@ void enterPlatformer(void)
 
         velocity.y += GRAVITY;
         playerPos.y += velocity.y;
-
         onGround = false;
 
         for (int i = 0; i < PLATFORM_COUNT; i++)
@@ -189,7 +231,7 @@ void enterPlatformer(void)
 
                     bool inDropZone = CheckCollisionRecs(playerRect, dropZone);
 
-                    if (inDropZone && IsKeyDown(KEY_S))
+                    if (inDropZone && IsKeyDown(KEY_S) && i != 0)
                         continue;
 
                     playerPos.y = platform.y - PLAYER_HEIGHT;
@@ -199,25 +241,119 @@ void enterPlatformer(void)
             }
         }
 
+        // Shooter update (STRONGER FIX)
+        for (int i = 0; i < PLATFORM_COUNT; i++)
+        {
+            if (!shooters[i].active) continue;
+
+            shooters[i].shootTimer++;
+
+            if (shooters[i].shootTimer >= SHOOT_INTERVAL)
+            {
+                shooters[i].shootTimer = 0;
+
+                bool fired = false;
+
+                for (int b = 0; b < MAX_BULLETS; b++)
+                {
+                    if (!bullets[b].active)
+                    {
+                        bullets[b].active = true;
+                        bullets[b].pos = (Vector2){
+                            shooters[i].rect.x + shooters[i].rect.width/2,
+                            shooters[i].rect.y + shooters[i].rect.height/2
+                        };
+                        bullets[b].direction = shooters[i].direction;
+                        fired = true;
+                        break;
+                    }
+                }
+
+                if (!fired)
+                {
+                    bullets[0].active = true;
+                    bullets[0].pos = (Vector2){
+                        shooters[i].rect.x + shooters[i].rect.width/2,
+                        shooters[i].rect.y + shooters[i].rect.height/2
+                    };
+                    bullets[0].direction = shooters[i].direction;
+                }
+            }
+        }
+
+        // Bullet update
+        for (int i = 0; i < MAX_BULLETS; i++)
+        {
+            if (!bullets[i].active) continue;
+
+            bullets[i].pos.x += BULLET_SPEED * bullets[i].direction;
+
+            if (bullets[i].pos.x < -2000 || bullets[i].pos.x > 4000)
+            {
+                bullets[i].active = false;
+                continue;
+            }
+
+            Rectangle bulletRect = {
+                bullets[i].pos.x - 5,
+                bullets[i].pos.y - 5,
+                10, 10
+            };
+
+            Rectangle playerRect = {
+                playerPos.x,
+                playerPos.y,
+                PLAYER_WIDTH,
+                PLAYER_HEIGHT
+            };
+
+            if (CheckCollisionRecs(bulletRect, playerRect))
+            {
+                GeneratePlatforms(platforms);
+                RegenerateShooters(platforms, shooters);
+
+                for (int j = 0; j < MAX_BULLETS; j++)
+                    bullets[j].active = false;
+
+                playerPos.x = platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2;
+                playerPos.y = platforms[0].rect.y - PLAYER_HEIGHT;
+                velocity = (Vector2){0,0};
+                onGround = true;
+                break;
+            }
+        }
+
         camera.target = playerPos;
 
         BeginDrawing();
         ClearBackground(DARKBLUE);
-        DrawFPS(100, 0);
-
+        DrawFPS(100,0);
 
         BeginMode2D(camera);
 
         for (int i = 0; i < PLATFORM_COUNT; i++)
         {
-            DrawRectangleRec(platforms[i].rect, GRAY);
-            DrawRectangleRec(platforms[i].dropZone, ORANGE);
+            if (i == 0)
+                DrawRectangleRec(platforms[i].rect, BLACK);
+            else if (i == PLATFORM_COUNT - 1)
+                DrawRectangleRec(platforms[i].rect, GOLD);
+            else
+                DrawRectangleRec(platforms[i].rect, GRAY);
+
+            if (i != 0)
+                DrawRectangleRec(platforms[i].dropZone, ORANGE);
         }
 
+        for (int i = 0; i < PLATFORM_COUNT; i++)
+            if (shooters[i].active)
+                DrawRectangleRec(shooters[i].rect, PURPLE);
+
+        for (int i = 0; i < MAX_BULLETS; i++)
+            if (bullets[i].active)
+                DrawCircle(bullets[i].pos.x, bullets[i].pos.y, 5, YELLOW);
+
         DrawRectangle(playerPos.x, playerPos.y,
-                      PLAYER_WIDTH,
-                      PLAYER_HEIGHT,
-                      RED);
+                      PLAYER_WIDTH, PLAYER_HEIGHT, RED);
 
         EndMode2D();
 
