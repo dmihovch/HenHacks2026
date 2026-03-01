14f6c92d0bfae7b281167b1d139d72b165d919a2
JACK MALLETT <JACKMUD@UDEL.EDU>
2026-02-28 19:11:53 -0500

    added particle effects to rectangle dudes

--- src/platformer.c
+++ src/platformer.c
@@ -21,6 +21,8 @@
 #define BULLET_SPEED 8
 #define SHOOT_INTERVAL 120
 
+#define MAX_PARTICLES 500
+
 typedef struct {
     Rectangle rect;
     Rectangle dropZone;
@@ -38,6 +40,15 @@ typedef struct {
     int direction;
     bool active;
 } Bullet;
+
+typedef struct {
+    Vector2 pos;
+    Vector2 velocity;
+    float radius;
+    Color color;
+    int life;
+    bool active;
+} Particle;
 
 // generate platforms
 void GeneratePlatforms(Platform platforms[])
@@ -112,6 +123,27 @@ void RegenerateShooters(Platform platforms[], Shooter shooters[])
     }
 }
 
+// Spawn particles for jumps
+void SpawnJumpParticles(Particle particles[], Vector2 pos)
+{
+    for (int p = 0; p < 1; p++) // p < k , k = particle amnt
+    {
+        for (int i = 0; i < MAX_PARTICLES; i++)
+        {
+            if (!particles[i].active)
+            {
+                particles[i].active = true;
+                particles[i].pos = (Vector2){ pos.x + PLAYER_WIDTH/2, pos.y + PLAYER_HEIGHT };
+                particles[i].radius = GetRandomValue(3,6);
+                particles[i].velocity = (Vector2){ GetRandomValue(-4,4), GetRandomValue(-7,-2) };
+                particles[i].color = (Color){ 200, 200, 200, 255 };
+                particles[i].life = 30;
+                break;
+            }
+        }
+    }
+}
+
 // when enter platformer..
 void enterPlatformer(void)
 {
@@ -120,18 +152,17 @@ void enterPlatformer(void)
     Platform platforms[PLATFORM_COUNT];
     Shooter shooters[PLATFORM_COUNT];
     Bullet bullets[MAX_BULLETS];
+    Particle particles[MAX_PARTICLES];
 
     GeneratePlatforms(platforms);
     RegenerateShooters(platforms, shooters);
 
-    for (int i = 0; i < MAX_BULLETS; i++)
-        bullets[i].active = false;
+    for (int i = 0; i < MAX_BULLETS; i++) bullets[i].active = false;
+    for (int i = 0; i < MAX_PARTICLES; i++) particles[i].active = false;
 
     Vector2 player1Pos = { platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2,
                             platforms[0].rect.y - PLAYER_HEIGHT };
-    Vector2 player2Pos = { platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2,
-                            platforms[0].rect.y - PLAYER_HEIGHT };
-
+    Vector2 player2Pos = player1Pos;
     Vector2 player1Vel = {0,0};
     Vector2 player2Vel = {0,0};
     bool player1OnGround = true;
@@ -142,21 +173,28 @@ void enterPlatformer(void)
     camera.offset = (Vector2){ SCREEN_WIDTH/2, SCREEN_HEIGHT/2 };
     camera.zoom = 1.0f;
 
-    float winTextTimer = 0.0f; // for floating text effect
+    float normalZoom = 1.0f;
+    float zoomedOut = 0.22f;
+    bool zoomToggled = false;
+    float zoomLerpSpeed = 0.05f;
+
+    float winTextTimer = 0.0f;
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
         if (IsKeyPressed(KEY_Q)) break;
 
+        // Toggle zoom with K key
+        if (IsKeyPressed(KEY_K)) zoomToggled = !zoomToggled;
+
         // respawn
         if (IsKeyPressed(KEY_R))
         {
             GeneratePlatforms(platforms);
             RegenerateShooters(platforms, shooters);
-
-            for (int i = 0; i < MAX_BULLETS; i++)
-                bullets[i].active = false;
+            for (int i = 0; i < MAX_BULLETS; i++) bullets[i].active = false;
+            for (int i = 0; i < MAX_PARTICLES; i++) particles[i].active = false;
 
             player1Pos.x = platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2;
             player1Pos.y = platforms[0].rect.y - PLAYER_HEIGHT;
@@ -171,19 +209,15 @@ void enterPlatformer(void)
         }
 
         Rectangle topPlatform = platforms[PLATFORM_COUNT-1].rect;
-
-        // Define player rectangles early for collision checks
         Rectangle player1Rect = { player1Pos.x, player1Pos.y, PLAYER_WIDTH, PLAYER_HEIGHT };
         Rectangle player2Rect = { player2Pos.x, player2Pos.y, PLAYER_WIDTH, PLAYER_HEIGHT };
 
-        // --- WIN CHECK ---
         bool player1OnTop = (player1OnGround && player1Pos.y + PLAYER_HEIGHT == topPlatform.y);
         bool player2OnTop = (player2OnGround && player2Pos.y + PLAYER_HEIGHT == topPlatform.y);
         bool win = player1OnTop && player2OnTop;
 
         if (!win)
         {
-            // --- NORMAL GAME UPDATES ---
             float prevY1 = player1Pos.y;
             float prevY2 = player2Pos.y;
 
@@ -193,15 +227,21 @@ void enterPlatformer(void)
             if (IsKeyDown(KEY_LEFT)) player2Pos.x -= PLAYER_SPEED;
             if (IsKeyDown(KEY_RIGHT)) player2Pos.x += PLAYER_SPEED;
 
-            if (IsKeyPressed(KEY_W) && player1OnGround) { player1Vel.y = JUMP_FORCE; player1OnGround = false; }
-            if (IsKeyPressed(KEY_UP) && player2OnGround) { player2Vel.y = JUMP_FORCE; player2OnGround = false; }
+            if (IsKeyPressed(KEY_W) && player1OnGround)
+            {
+                player1Vel.y = JUMP_FORCE; player1OnGround = false;
+                SpawnJumpParticles(particles, player1Pos);
+            }
 
-            player1Vel.y += GRAVITY;
-            player2Vel.y += GRAVITY;
-            player1Pos.y += player1Vel.y;
-            player2Pos.y += player2Vel.y;
-            player1OnGround = false;
-            player2OnGround = false;
+            if (IsKeyPressed(KEY_UP) && player2OnGround)
+            {
+                player2Vel.y = JUMP_FORCE; player2OnGround = false;
+                SpawnJumpParticles(particles, player2Pos);
+            }
+
+            player1Vel.y += GRAVITY; player2Vel.y += GRAVITY;
+            player1Pos.y += player1Vel.y; player2Pos.y += player2Vel.y;
+            player1OnGround = false; player2OnGround = false;
 
             // platform collision & score
             for (int i = 0; i < PLATFORM_COUNT; i++)
@@ -209,7 +249,6 @@ void enterPlatformer(void)
                 Rectangle plat = platforms[i].rect;
                 Rectangle dropZone = platforms[i].dropZone;
 
-                // player 1
                 if (player1Vel.y > 0)
                 {
                     float prevBot = prevY1 + PLAYER_HEIGHT;
@@ -229,7 +268,6 @@ void enterPlatformer(void)
                     }
                 }
 
-                // player 2
                 if (player2Vel.y > 0)
                 {
                     float prevBot = prevY2 + PLAYER_HEIGHT;
@@ -289,80 +327,102 @@ void enterPlatformer(void)
                     GeneratePlatforms(platforms);
                     RegenerateShooters(platforms, shooters);
                     for (int j = 0; j < MAX_BULLETS; j++) bullets[j].active = false;
+                    for (int j = 0; j < MAX_PARTICLES; j++) particles[j].active = false;
 
                     player1Pos.x = platforms[0].rect.x + PLATFORM_WIDTH/2 - PLAYER_WIDTH/2;
                     player1Pos.y = platforms[0].rect.y - PLAYER_HEIGHT;
                     player2Pos = player1Pos;
-
-                    player1Vel = (Vector2){0,0};
-                    player2Vel = (Vector2){0,0};
+                    player1Vel = player2Vel = (Vector2){0,0};
                     player1OnGround = player2OnGround = true;
-
                     score = 0;
                     break;
                 }
             }
+        }
 
-            camera.target.x = (player1Pos.x + player2Pos.x)/2;
-            camera.target.y = (player1Pos.y + player2Pos.y)/2;
+        // Update particles
+        for (int i = 0; i < MAX_PARTICLES; i++)
+        {
+            if (!particles[i].active) continue;
+            particles[i].pos.x += particles[i].velocity.x;
+            particles[i].pos.y += particles[i].velocity.y;
+            particles[i].velocity.y += 0.5f; // gravity
+            particles[i].life--;
+            if (particles[i].life <= 0) particles[i].active = false;
+        }
+
+        // --- CAMERA LOGIC ---
+        if (zoomToggled)
+        {
+            float minX = platforms[0].rect.x;
+            float maxX = platforms[0].rect.x + platforms[0].rect.width;
+            float minY = platforms[0].rect.y;
+            float maxY = platforms[0].rect.y + platforms[0].rect.height;
+
+            for (int i = 1; i < PLATFORM_COUNT; i++)
+            {
+                if (platforms[i].rect.x < minX) minX = platforms[i].rect.x;
+                if (platforms[i].rect.x + platforms[i].rect.width > maxX) maxX = platforms[i].rect.x + platforms[i].rect.width;
+                if (platforms[i].rect.y < minY) minY = platforms[i].rect.y;
+                if (platforms[i].rect.y + platforms[i].rect.height > maxY) maxY = platforms[i].rect.y + platforms[i].rect.height;
+            }
+
+            camera.zoom += (zoomedOut - camera.zoom) * zoomLerpSpeed;
+            camera.target.x += ((minX + maxX)/2 - camera.target.x) * zoomLerpSpeed;
+            camera.target.y += ((minY + maxY)/2 - camera.target.y) * zoomLerpSpeed;
         }
         else
         {
-            // --- WIN SCREEN CAMERA ---
-            camera.target.x = topPlatform.x + topPlatform.width/2;
-            camera.target.y = topPlatform.y + topPlatform.height/2;
-            winTextTimer += 0.05f;
+            camera.zoom += (normalZoom - camera.zoom) * zoomLerpSpeed;
+            if (!win)
+            {
+                camera.target.x += ((player1Pos.x + player2Pos.x)/2 - camera.target.x) * zoomLerpSpeed;
+                camera.target.y += ((player1Pos.y + player2Pos.y)/2 - camera.target.y) * zoomLerpSpeed;
+            }
         }
 
         // --- DRAWING ---
         BeginDrawing();
         ClearBackground(DARKBLUE);
 
-        if (win)
+        BeginMode2D(camera);
+        for (int i = 0; i < PLATFORM_COUNT; i++)
         {
-            // Dimmed background
-            DrawRectangle(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,(Color){0,0,0,150});
+            if (i == 0) DrawRectangleRec(platforms[i].rect, BLACK);
+            else if (i == PLATFORM_COUNT-1) DrawRectangleRec(platforms[i].rect, GOLD);
+            else DrawRectangleRec(platforms[i].rect, GRAY);
 
-            // Floating "YOU WIN!" text
-            int floatOffset = (int)(10 * sin(winTextTimer));
-            DrawText("YOU WIN!", SCREEN_WIDTH/2 - 120, SCREEN_HEIGHT/2 - 40 + floatOffset, 60, GOLD);
-            DrawText("Press R to Play Again or Q to Quit", SCREEN_WIDTH/2 - 220, SCREEN_HEIGHT/2 + 50 + floatOffset, 30, WHITE);
-
-            // Players hidden on win screen
+            if (i != 0) DrawRectangleRec(platforms[i].dropZone, ORANGE);
+            if (shooters[i].active) DrawRectangleRec(shooters[i].rect, PURPLE);
         }
-        else
+
+        for (int i = 0; i < MAX_BULLETS; i++)
+            if (bullets[i].active) DrawCircle(bullets[i].pos.x, bullets[i].pos.y, 5, YELLOW);
+
+        // Draw particles
+        for (int i = 0; i < MAX_PARTICLES; i++)
+            if (particles[i].active) DrawCircleV(particles[i].pos, particles[i].radius, particles[i].color);
+
+        if (!win)
         {
-            BeginMode2D(camera);
-
-            for (int i = 0; i < PLATFORM_COUNT; i++)
-            {
-                if (i == 0)
-                    DrawRectangleRec(platforms[i].rect, BLACK);
-                else if (i == PLATFORM_COUNT-1)
-                    DrawRectangleRec(platforms[i].rect, GOLD);
-                else
-                    DrawRectangleRec(platforms[i].rect, GRAY);
-
-                if (i != 0)
-                    DrawRectangleRec(platforms[i].dropZone, ORANGE);
-
-                if (shooters[i].active)
-                    DrawRectangleRec(shooters[i].rect, PURPLE);
-            }
-
-            for (int i = 0; i < MAX_BULLETS; i++)
-                if (bullets[i].active)
-                    DrawCircle(bullets[i].pos.x, bullets[i].pos.y, 5, YELLOW);
-
             DrawRectangle(player1Pos.x, player1Pos.y, PLAYER_WIDTH, PLAYER_HEIGHT, RED);
             DrawRectangle(player2Pos.x, player2Pos.y, PLAYER_WIDTH, PLAYER_HEIGHT, BLUE);
-
-            EndMode2D();
-
-            DrawText(TextFormat("Score: %i", score), 20, 20, 20, WHITE);
-            DrawText("Q: Lobby | R: Respawn | S/DOWN: Drop | WASD: P1 | Arrows: P2", 20, 50, 20, WHITE);
         }
 
+        EndMode2D();
+
+        if (win)
+        {
+            int floatOffset = (int)(10 * sin(winTextTimer));
+            DrawRectangle(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,(Color){0,0,0,150});
+            DrawText("YOU WIN!", SCREEN_WIDTH/2 - 120, SCREEN_HEIGHT/2 - 40 + floatOffset, 60, GOLD);
+            DrawText("Press R to Play Again or Q to Quit", SCREEN_WIDTH/2 - 220, SCREEN_HEIGHT/2 + 50 + floatOffset, 30, WHITE);
+        }
+
+        DrawText(TextFormat("Score: %i", score), 20, 20, 20, WHITE);
+        DrawText("Q: Lobby | R: Respawn | S/DOWN: Drop | WASD: P1 | Arrows: P2 | K: Zoom Out", 20, 50, 20, WHITE);
+
+        winTextTimer += 0.05f;
         EndDrawing();
     }
 }
